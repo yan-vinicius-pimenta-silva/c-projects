2e88388e77b240e2212bb2819e59a542057baa4f
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-13 20:34:00 -0300

    new things learned

--- C Files/ternary-operator.c
+++ C Files/ternary-operator.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 
-int main() {
+
+
+int findMax(int x, int y)
+{
+
+    // if(x>y)
+    // {
+    //     return x;
+    // }
+    // else 
+    // {
+    //     return y;
+    // }
+
+    // The if/else statement above can be substituted by the following ternary statement:
+    // return (x > y) ? x : y ;
+    return (x > y) ? x : y;
+
+    // Traduzindo : retorne o valor de 'x' se 'x' for maior que 'y', caso contrário, retorne o valor de 'y'.
+
+}
+
+int main() 
+
+{
 
     // ternary operator = shortcut to if/else when assigning/returning a value
     // (condition) ? value if true : value if false
 
+    int max = findMax(5, 4);
+
+    printf("%d",max);
+
+
     return 0;
+
 }
