55e181a11a8f1ffd4e1c2ad8b36c4b1027197628
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-14 20:30:36 -0300

    new things

---
+++ C Files/string-functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char string1[] = "Bro";
+    char string2[] = "Code";
+
+    //strlwr(string1);             // converts a string to lowercase
+    //strupr(string1);             // converts a string to uppercase
+    //strcat(string1,string2);     // appends string2 to end of string1
+    //strncat(string1,string2,1);  // appends n characters from string2 to 
+    //strcpy(string1,string2);     // copy string2 to string1
+    //strncpy(string1,string2,4);  // copy n characters of string2 to string1
+
+    printf("%s",string1);
+
+    //14-1-2025
+
+}
