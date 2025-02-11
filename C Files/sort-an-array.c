8918f227964c0532807414557e22aa2d68c802ae
Yan <yan.fedora@gmail.com>
2025-02-11 17:34:04 -0300

    new features

--- C Files/sort-an-array.c
+++ C Files/sort-an-array.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void sort(int array[], int size){
+void sort(char array[], int size){
     for(int i = 0; i < size - 1; i++){
         for(int j = 0; j < size - 1; j++){
             if(array[j] > array[j+1]){
-                int temp = array[j];
+                char temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
             }
@@ -12,11 +12,22 @@ void sort(int array[], int size){
     }
 
 }
+
+void printArray(char array[], int size){
+    for(int i = 0; i < size; i++){
+        printf("%c ", array[i]);
+    }
+}
+
 int main(){
 
-    int array[] = {9,1,8,2,7,3,6,4,5};
+
+    char array[] = {'F','A','D','B','C'};
+    // int array[] = {9,1,8,2,7,3,6,4,5};
     int size = sizeof(array)/sizeof(array[0]);
 
     sort(array,size);
+    printArray(array,size);
+
     return 0;
 }   
