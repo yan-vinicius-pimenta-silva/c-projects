ece52bf2e57c8a92577df508a0caf22712a28fec
Yan <yan.fedora@gmail.com>
2025-03-23 19:53:53 -0300

    new files

--- C Files/07-03-2025/1.c
+++ C Files/07-03-2025/1.c
@@ -5,6 +5,16 @@
 
 int main(){
 
+    // 1) Ler um número inteiro n. Escrever a soma de todos os números de 1 até n.
+
+    int x;
+    scanf("%d",&x);
+    int soma = 0;
+    for(int i = 1;i <= x;i++){
+        soma = soma + i;
+    }
+
+    printf("O valor da soma é %d",soma);
 
     return 0;
 }
