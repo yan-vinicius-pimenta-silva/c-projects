6247e8b1d0d6fff3d884a5fb0ea4ec6728dbe853
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-04-09 09:16:52 -0300

    teste

--- C Files/beecrowd-problems-beginner/40-average3.c
+++ C Files/beecrowd-problems-beginner/40-average3.c
@@ -2,18 +2,41 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+
 int main(){
 
   float n1,n2,n3,n4;
   scanf("%f %f %f %f",&n1,&n2,&n3,&n4);
 
   float average = ((n1*2)+(n2*3)+(n3*4)+(n4*1))/10;
-  printf("Media: %.1f",average);
+  printf("Media: %.1f\n",average);
   
 
-  if(){
-    
+  if(average >= 5.0 && average <= 6.9){
+    printf("Aluno em exame.\n");
+    float exame;
+    scanf("%f",&exame);
+    printf("Nota do exame: %.1f\n",exame);
+    float newAverage = (average+exame)/2;
+    if(newAverage >= 5.0){
+      printf("Aluno aprovado.\n");
+      
+
+
+    } else if(newAverage <= 4.9){
+      printf("Aluno reprovado.\n");
+    }
+    printf("Media final: %.1f\n",newAverage);
+
   }
+  else if(average >= 7.0){
+    printf("Aluno aprovado.\n");
+  }else if(average < 5.0){
+    printf("Aluno reprovado.\n");
+  }
+
+
 
 
 
